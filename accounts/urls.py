cc64d333de685efa8f7ee0e213084390fdc9ecf8
HeureuseD <da4260@daum.net>
2024-11-10 03:07:59 +0900

    feat: token refresh url  추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView 
-from .views import SignupAPIView, LogoutView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import SignupAPIView, LogoutAPIView
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
     path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
