d5e257c6656656dfad2e499835162fa7fc712a07
HeureuseD <da4260@daum.net>
2024-11-10 03:05:09 +0900

    feat: logout url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView 
-from .views import SignupAPIView
+from .views import SignupAPIView, LogoutView
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
     path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
