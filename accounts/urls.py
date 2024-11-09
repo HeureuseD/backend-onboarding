f86507a93186dacb3b5ccbb47d83b0c6d8521be1
HeureuseD <da4260@daum.net>
2024-11-10 03:02:35 +0900

    feat: 로그인 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView 
 from .views import SignupAPIView
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
+    path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
