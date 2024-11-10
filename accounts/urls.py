a0fa79f62fab2bb041bb1fcfc3891b0d340f2210
HeureuseD <da4260@daum.net>
2024-11-10 22:16:02 +0900

    feat: 새로운 로그인 뷰로 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignupAPIView, LogoutAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import SignupAPIView, LogoutAPIView, CustomTokenObtainPairView
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
-    path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("signin/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
